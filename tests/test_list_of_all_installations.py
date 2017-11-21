13c7df25006479b1b0884e78f1b4a1768b873286
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2017-11-21 13:50:23 +0100

    tests: Updating test code for registry lookup

--- tests/test_list_of_all_installations.py
+++ tests/test_list_of_all_installations.py
@@ -1,12 +1,28 @@
 import winreg
 
-parentKey = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, None)
-i = 0
-while True:
-    try:
-        key = winreg.EnumKey(parentKey, i)
-        if key.startswith("SldWorks.Application"):
-            print(key)
-        i += 1
-    except WindowsError: 
-        break
+class Test():
+    def __init__(self):
+        self._default_app_name = "SldWorks.Application"
+    
+    def getVersionedInstallationsFromRrgistry(self):
+        versions = []
+        registered_services = winreg.OpenKey(winreg.HKEY_CLASSES_ROOT, None)
+        key_prefix = "{}.".format(self._default_app_name)
+        i = 0
+        while True:
+            try:
+                key = winreg.EnumKey(registered_services, i)
+                if key.startswith(key_prefix):
+                    try:
+                        major_version = key[len(key_prefix):]
+                        major_version = int(major_version)
+                        versions.append(major_version)
+                    except ValueError:
+                        pass
+                i += 1
+            except WindowsError: 
+                break
+        return versions
+
+if __name__ == "__main__":
+    print(Test().getVersionedInstallationsFromRrgistry())
