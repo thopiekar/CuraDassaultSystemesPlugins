28c37e6558706e491627c30d3d9dc25581fc2585
Thomas Karl Pietrowski <thopiekar@googlemail.com>
2018-02-13 23:32:34 +0100

    SystemUtils: Replacing code with win32api
    The ctypes code gives too less tracebacks. They get somehow lost and it is too difficult to debug.

--- SystemUtils.py
+++ SystemUtils.py
@@ -1,11 +1,15 @@
 # Copyright (c) 2017 Thomas Karl Pietrowski
 
-import ctypes
+#import ctypes
+import win32api
 
 def convertDosPathIntoLongPath(dosPath):
-    buffer = ctypes.create_unicode_buffer(1024)
-    if not ctypes.windll.kernel32.GetLongPathNameW(dosPath, buffer, 1024): # GetLongPathNameW: @UndefinedVariable
-        # This basically indicates that the call of the function failed, since nothing has been passed to the buffer.
-        # It is better to catch these situations and raise an error here!
-        raise ValueError("Bad path passed!")
-    return buffer.value
+    # TODO: Move the code into the COM compat layer!
+    #longpath = ctypes.create_unicode_buffer(1024)
+    #if not ctypes.windll.kernel32.GetLongPathNameW(dosPath, longpath, 1024): # GetLongPathNameW: @UndefinedVariable
+    #    # This basically indicates that the call of the function failed, since nothing has been passed to the buffer.
+    #    # It is better to catch these situations and raise an error here!
+    #    raise ValueError("Bad path passed!")
+    #return longpath.value
+    longpath = win32api.GetLongPathName(dosPath)
+    return longpath
